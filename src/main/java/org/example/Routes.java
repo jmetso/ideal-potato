938d82f6501d2b8d264ca6fd43b507b7adb36657
jmetso <jmetso@redhat.com>
2021-11-03 16:13:44 +0200

    adds env variable to timeout delay

--- src/main/java/org/example/Routes.java
+++ src/main/java/org/example/Routes.java
@@ -36,7 +36,7 @@ public class Routes extends RouteBuilder {
 
         from("direct:timeout")
                 .log("direct:timeout")
-                .delay(120000)
+                .delay(Integer.parseInt("{{TIMEOUT_DELAY:120000}}"))
                 .to("direct:hello");
 
     }
